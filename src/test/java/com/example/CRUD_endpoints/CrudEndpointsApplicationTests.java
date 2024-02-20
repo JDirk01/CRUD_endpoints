13fb9f66df3bc206dd826a057e91e21095fe5ab8
JDirk01 <jarrendirk4@gmail.com>
2024-02-20 11:03:59 -0800

    Base Spring project

---
+++ src/test/java/com/example/CRUD_endpoints/CrudEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
